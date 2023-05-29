8d38750755aa25b36717e751594fb107c2a73723
Samson Ajayi <Samson.Ajayi@sterling.ng>
2023-05-29 01:50:25 +0100

    Add project files.

---
+++ Consumer/Reciever.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+public class Reciever
+{
+    public static void Main(string[] args)
+    {
+        ConnectionFactory factory = new();
+        factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Recieved message {0}.....",message);
+            };
+
+            channel.BasicConsume("BasicTest", true, consumer);
+
+            Console.WriteLine("Press enter to exit Consumer");
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+
+public class Sender
+{
+    public static void Main(string[] args)
+    {
+        ConnectionFactory factory = new();
+        factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+
+        using (var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest",false,false,false,null);
+
+            string message = "Getting started with .Net Core RabbitMQ";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", "BasicTest", null, body);
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
